4065a01d54b8914e529b863cb77863fc5fa21acc
Uzoma Emuchay <uzzije2000@yahoo.co.uk>
2023-06-07 20:13:27 -0500

    update formatting

--- whisperx/vad.py
+++ whisperx/vad.py
@@ -18,7 +18,14 @@ from .diarize import Segment as SegmentX
 VAD_SEGMENTATION_URL = "https://whisperx.s3.eu-west-2.amazonaws.com/model_weights/segmentation/0b5b3216d60a2d32fc086b47ea8c67589aaeb26b7e07fcbe620d6d0b83e209ea/pytorch_model.bin"
 
 
-def load_vad_model(device, vad_onset=0.500, vad_offset=0.363, use_auth_token=None, model_fp=None, **kwargs):
+def load_vad_model(
+    device,
+    vad_onset=0.500,
+    vad_offset=0.363,
+    use_auth_token=None,
+    model_fp=None,
+    **kwargs,
+):
     model_dir = torch.hub._get_torch_home()
     os.makedirs(model_dir, exist_ok=True)
     if model_fp is None:
@@ -27,13 +34,15 @@ def load_vad_model(device, vad_onset=0.500, vad_offset=0.363, use_auth_token=Non
         raise RuntimeError(f"{model_fp} exists and is not a regular file")
 
     if not os.path.isfile(model_fp):
-        with urllib.request.urlopen(VAD_SEGMENTATION_URL) as source, open(model_fp, "wb") as output:
+        with urllib.request.urlopen(VAD_SEGMENTATION_URL) as source, open(
+            model_fp, "wb"
+        ) as output:
             with tqdm(
-                    total=int(source.info().get("Content-Length")),
-                    ncols=80,
-                    unit="iB",
-                    unit_scale=True,
-                    unit_divisor=1024,
+                total=int(source.info().get("Content-Length")),
+                ncols=80,
+                unit="iB",
+                unit_scale=True,
+                unit_divisor=1024,
             ) as loop:
                 while True:
                     buffer = source.read(8192)
@@ -44,19 +53,24 @@ def load_vad_model(device, vad_onset=0.500, vad_offset=0.363, use_auth_token=Non
                     loop.update(len(buffer))
 
     model_bytes = open(model_fp, "rb").read()
-    if hashlib.sha256(model_bytes).hexdigest() != VAD_SEGMENTATION_URL.split('/')[-2]:
+    if hashlib.sha256(model_bytes).hexdigest() != VAD_SEGMENTATION_URL.split("/")[-2]:
         raise RuntimeError(
             "Model has been downloaded but the SHA256 checksum does not not match. Please retry loading the model."
         )
 
     vad_model = Model.from_pretrained(model_fp, use_auth_token=use_auth_token)
-    hyperparameters = {"onset": vad_onset,
-                       "offset": vad_offset,
-                       "min_duration_on": 0.1,
-                       "min_duration_off": 0.1}
+    hyperparameters = {
+        "onset": vad_onset,
+        "offset": vad_offset,
+        "min_duration_on": 0.1,
+        "min_duration_off": 0.1,
+    }
 
-    vad_pipeline = VoiceActivitySegmentation(segmentation=vad_model, device=torch.device(device),
-                                             **kwargs.get('inference_kwargs', {}))
+    vad_pipeline = VoiceActivitySegmentation(
+        segmentation=vad_model,
+        device=torch.device(device),
+        **kwargs.get("inference_kwargs", {}),
+    )
     vad_pipeline.instantiate(hyperparameters)
 
     return vad_pipeline
@@ -96,16 +110,15 @@ class Binarize:
     """
 
     def __init__(
-            self,
-            onset: float = 0.5,
-            offset: Optional[float] = None,
-            min_duration_on: float = 0.0,
-            min_duration_off: float = 0.0,
-            pad_onset: float = 0.0,
-            pad_offset: float = 0.0,
-            max_duration: float = float('inf')
+        self,
+        onset: float = 0.5,
+        offset: Optional[float] = None,
+        min_duration_on: float = 0.0,
+        min_duration_off: float = 0.0,
+        pad_onset: float = 0.0,
+        pad_offset: float = 0.0,
+        max_duration: float = float("inf"),
     ):
-
         super().__init__()
 
         self.onset = onset
@@ -138,7 +151,6 @@ class Binarize:
         # annotation meant to store 'active' regions
         active = Annotation()
         for k, k_scores in enumerate(scores.data.T):
-
             label = k if scores.labels is None else scores.labels[k]
 
             # initial state
@@ -153,13 +165,17 @@ class Binarize:
                     if curr_duration > self.max_duration:
                         search_after = len(curr_scores) // 2
                         # divide segment
-                        min_score_div_idx = search_after + np.argmin(curr_scores[search_after:])
+                        min_score_div_idx = search_after + np.argmin(
+                            curr_scores[search_after:]
+                        )
                         min_score_t = curr_timestamps[min_score_div_idx]
-                        region = Segment(start - self.pad_onset, min_score_t + self.pad_offset)
+                        region = Segment(
+                            start - self.pad_onset, min_score_t + self.pad_offset
+                        )
                         active[region, k] = label
                         start = curr_timestamps[min_score_div_idx]
-                        curr_scores = curr_scores[min_score_div_idx + 1:]
-                        curr_timestamps = curr_timestamps[min_score_div_idx + 1:]
+                        curr_scores = curr_scores[min_score_div_idx + 1 :]
+                        curr_timestamps = curr_timestamps[min_score_div_idx + 1 :]
                     # switching from active to inactive
                     elif y < self.offset:
                         region = Segment(start - self.pad_onset, t + self.pad_offset)
@@ -186,7 +202,9 @@ class Binarize:
         # also: fill same speaker gaps shorter than min_duration_off
         if self.pad_offset > 0.0 or self.pad_onset > 0.0 or self.min_duration_off > 0.0:
             if self.max_duration < float("inf"):
-                raise NotImplementedError(f"This would break current max_duration param")
+                raise NotImplementedError(
+                    f"This would break current max_duration param"
+                )
             active = active.support(collar=self.min_duration_off)
 
         # remove tracks shorter than min_duration_on
@@ -200,14 +218,18 @@ class Binarize:
 
 class VoiceActivitySegmentation(VoiceActivityDetection):
     def __init__(
-            self,
-            segmentation: PipelineModel = "pyannote/segmentation",
-            fscore: bool = False,
-            use_auth_token: Union[Text, None] = None,
-            **inference_kwargs,
+        self,
+        segmentation: PipelineModel = "pyannote/segmentation",
+        fscore: bool = False,
+        use_auth_token: Union[Text, None] = None,
+        **inference_kwargs,
     ):
-
-        super().__init__(segmentation=segmentation, fscore=fscore, use_auth_token=use_auth_token, **inference_kwargs)
+        super().__init__(
+            segmentation=segmentation,
+            fscore=fscore,
+            use_auth_token=use_auth_token,
+            **inference_kwargs,
+        )
 
     def apply(self, file: AudioFile, hook: Optional[Callable] = None) -> Annotation:
         """Apply voice activity detection
@@ -243,7 +265,9 @@ class VoiceActivitySegmentation(VoiceActivityDetection):
         return segmentations
 
 
-def merge_vad(vad_arr, pad_onset=0.0, pad_offset=0.0, min_duration_off=0.0, min_duration_on=0.0):
+def merge_vad(
+    vad_arr, pad_onset=0.0, pad_offset=0.0, min_duration_off=0.0, min_duration_on=0.0
+):
     active = Annotation()
     for k, vad_t in enumerate(vad_arr):
         region = Segment(vad_t[0] - pad_onset, vad_t[1] + pad_offset)
@@ -259,7 +283,7 @@ def merge_vad(vad_arr, pad_onset=0.0, pad_offset=0.0, min_duration_off=0.0, min_
                 del active[segment, track]
 
     active = active.for_json()
-    active_segs = pd.DataFrame([x['segment'] for x in active['content']])
+    active_segs = pd.DataFrame([x["segment"] for x in active["content"]])
     return active_segs
 
 
@@ -288,11 +312,13 @@ def merge_chunks(segments, chunk_size):
 
     for seg in segments_list:
         if seg.end - curr_start > chunk_size and curr_end - curr_start > 0:
-            merged_segments.append({
-                "start": curr_start,
-                "end": curr_end,
-                "segments": seg_idxs,
-            })
+            merged_segments.append(
+                {
+                    "start": curr_start,
+                    "end": curr_end,
+                    "segments": seg_idxs,
+                }
+            )
             curr_start = seg.start
             seg_idxs = []
             speaker_idxs = []
@@ -300,9 +326,11 @@ def merge_chunks(segments, chunk_size):
         seg_idxs.append((seg.start, seg.end))
         speaker_idxs.append(seg.speaker)
     # add final
-    merged_segments.append({
-        "start": curr_start,
-        "end": curr_end,
-        "segments": seg_idxs,
-    })
+    merged_segments.append(
+        {
+            "start": curr_start,
+            "end": curr_end,
+            "segments": seg_idxs,
+        }
+    )
     return merged_segments
